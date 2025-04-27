fb1112edbf16919d96a4480a8e74bc50e945b2ff
Itamar Tziger <i0546746147@gmail.com>
2025-04-27 21:55:16 +0300

    Example for add Load Balance in Digital Ocean

---
+++ DigitalOcean/Examples/LoadBalance/main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+data "digitalocean_droplet" "splidx01" {
+  tag = "splidx01"
+}
+output "droplet_splidx01" {
+  value = data.digitalocean_droplet.splidx01.*
+}
+
+data "digitalocean_droplet" "splidx02" {
+  tag = "splidx02"
+}
+output "droplet_splidx02" {
+  value = data.digitalocean_droplet.splidx02.*
+}
+
+resource "digitalocean_loadbalancer" "public" {
+  name   = "splunk-lb-1"
+  region = "nyc3"
+
+  forwarding_rule {
+    entry_port     = 8443
+    entry_protocol = "http"
+
+    target_port     = 8000
+    target_protocol = "http"
+  }
+
+  healthcheck {
+    port     = 8000
+    protocol = "tcp"
+  }
+
+   sticky_sessions {
+    type = "cookies"
+    cookie_name = "splunk"
+    cookie_ttl_seconds = 3600
+  }
+
+  droplet_ids = [data.digitalocean_droplet.splidx01.id, data.digitalocean_droplet.splidx02.id]
+}
+output "loadbalancer_public" {
+  value = digitalocean_loadbalancer.public.*
+}
+
+## DNS variables
+variable "domain_name" {
+  description = "Domain name for the droplet"
+  type        = string
+  default     = "0546746147.com"
+}
+
+data "digitalocean_domain" "domain" {
+  name = var.domain_name  
+}
+
+resource "digitalocean_record" "lb_record" {
+  domain = var.domain_name
+  name   = "splunk"
+  type   = "A"
+  value  = digitalocean_loadbalancer.public.ip
+  ttl    = 3600
+  depends_on = [ digitalocean_loadbalancer.public ]
+}
+output "lb_record" {
+  value = digitalocean_record.lb_record.fqdn
+}
