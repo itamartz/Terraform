1d7361f1d414525edb4b094f33f45a5e547307fd
Itamar Tziger <i0546746147@gmail.com>
2025-04-27 20:16:05 +0300

    add output to example

--- DigitalOcean/Examples/Droplet/main.tf
+++ DigitalOcean/Examples/Droplet/main.tf
@@ -1,6 +1,9 @@
-module "droplet" {
+module "digitalocean_droplet_splcms" {
   source               =  "../../modules/droplet"
   server_name          = "splcms"
   digitalocean_ssh_id = "46839414" # doctl compute ssh-key list --format ID,Name --no-header
   server_role          = "cms"
+}
+output "digitalocean_droplet_splcms" {
+  value = module.digitalocean_droplet_splcms.*
 }
